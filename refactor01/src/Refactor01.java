1a26be445b92062f8aa29f87f1795775d1109762
Jorge Lopez Gil <wiijlg@hotmail.com>
2016-11-10 20:01:25 +0100

    v1.1

--- refactor01/src/Refactor01.java
+++ refactor01/src/Refactor01.java
@@ -1,18 +1,13 @@
 public class Refactor01 {
 	public static void main(String[] args) {		
 		int lado=3, radio=3, base=3, altura=6;	//VARIABLES
-		float area, pi=3.14f;
-		area=lado*lado;
-	
-		Imprime("CUADRADO", area);		//Imprimir por pantalla
-		area=pi*radio*radio;
-		Imprime("CIRCULO", area);
-		area=base*altura;
-		Imprime("RECTANGULO", area);
-	}
+		float area, pi=3.14f;					//VARIABLES
+		area=lado*lado;				//Calculo
+		Imprime("CUADRADO", area);		//Imprime en pantalla
+		area=pi*radio*radio;		//Calculo
+		Imprime("CIRCULO", area);		//Imprime en pantalla
+		area=base*altura;			//Calculo
+		Imprime("RECTANGULO", area);	//Imprime en pantalla
+		}
 	public static void Imprime(String tipo, float area){
-		System.out.println("---- AREA DE UN " + tipo + " ------");
-		System.out.println(area);
-		System.out.println("-------------------------------");
-	}
-}
+		System.out.println("---- AREA DE UN " + tipo + " ------ \n" + area + "\n" + "-------------------------------");}}
